f34318b0c042c47a1cfe7bf68ee75ad9887ad042
luhaitingfeng <1579198098@qq.com>
2019-11-28 15:33:55 +0800

    initial

---
+++ backend/src/main/java/com/moving/backend/controller/PetController.java
@@ -0,0 +1,16 @@
+package com.moving.backend.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <h3>com.moving.backend.controller</h3>
+ * <p>Resolve Pet Data</p>
+ *
+ * @author : Luhaitingfeng
+ * @date : 2019-11-28 15:11
+ **/
+@Controller
+public class PetController {
+
+
+}
